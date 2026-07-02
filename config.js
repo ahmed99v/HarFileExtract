2ad2983b480c62780e9ee3d02aac02158ec08ff7
ahmed <ahmedforneas99v@gmail.com>
2026-07-01 23:16:03 -0400

    Annotate about the config variables

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const CONFIG = {
     inputDir: path.resolve(__dirname, 'input'),
     outputBaseDir: path.resolve(__dirname, 'output'),
-    removeZeroByteFiles: true,
+    removeZeroByteFiles: true,	//Remove the zero files 
     maxFilenameLength: 250, // Windows max filename length
-    invalidChars: /[<>:"|?*%,!&()]/g,
+    invalidChars: /[<>:"|?*%,!&()]/g,		//Invalid characters
     replacementChar: '-',
   };
 
